4f29ff81ae707f14d8b64ecb505994900148b962
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-10-30 09:44:06 +0900

    演習問題2-7追加

---
+++ Ex_03_07/Modules/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_03_07.Modules;
+
+public class Item
+{
+    public int ItemNo { get; set; } // 商品番号を表すプロパティ
+    public string ItemName { get; set; } = ""; // 商品名を表すプロパティ
+    public int Price { get; set; } // 単価を表すプロパティ
+    /// <summary>
+    /// 各プロパティの値を表示
+    /// </summary>
+    public void Print()
+    {
+        Console.WriteLine("商品番号 = "+ ItemNo);
+        Console.WriteLine("商品名 = " + ItemName);
+        Console.WriteLine("単価 = " + Price);
+    }
+}
---
+++ Ex_03_07/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_03_07.Modules;
+
+namespace Ex_03_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item = new Item();
+        Console.Write("商品番号を入力してください->");
+        item.ItemNo = int.Parse(Console.ReadLine()!);
+        Console.Write("商品名を入力してください->");
+        item.ItemName = Console.ReadLine()!;
+        Console.Write("単価を入力してください->");
+        item.Price = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("結果");
+        item.Print();
+    }
+}
