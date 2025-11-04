323c5d9bd51f0adcb2b2fdd750e9667ded9ac35e
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-11-04 10:14:31 +0900

    演習問題-13追加

---
+++ Ex_03_13/Circle.cs
@@ -0,0 +1,40 @@
+﻿namespace Ex_03_13;
+
+/// <summary>
+/// 円を表すクラス
+/// </summary>
+public class Circle
+{
+    // 円周率
+    public static double PI = 3.14;
+    //  半径
+    int radius;
+
+    /// <summary>
+    /// フィールドの半径の値の円の面積を計算する動的メソッド
+    /// </summary>
+    /// <returns></returns>
+    public double AreaCalculation()
+    {
+        return radius * radius * PI;
+    }
+
+    /// <summary>
+    /// 引数の半径の値の円の面積を計算する静的メソッド
+    /// </summary>
+    /// <param name="radius">半径</param>
+    /// <returns></returns>
+    public static double AreaCalculation(int radius)
+    {
+        return radius * radius * PI;
+    }
+    
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="pradius">半径</param>
+    public Circle(int pradius)
+    {
+        radius = pradius;
+    }
+}
---
+++ Ex_03_13/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex_03_13;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Console.WriteLine($"5 * 5 * 3.14 = {circle.AreaCalculation()}");
+        Console.WriteLine($"5 * 5 * 3.14 = {Circle.AreaCalculation(5)}" );
+    }
+}
