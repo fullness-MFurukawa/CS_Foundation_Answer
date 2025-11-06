482c3788bc5609664fcafc8731b25c7d5b899ded
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-11-06 09:00:34 +0900

    演習04-01と演習04-01ライブラリプロジェクトを追加

---
+++ Ex_04_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ex_04_01_Lib;
+namespace Ex_04_01;
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Item item = new Item();
+        Console.Write("商品番号を入力してください->");
+        item.ItemNo = int.Parse(Console.ReadLine()!);
+        Console.Write("商品名を入力してください->");
+        item.ItemName = Console.ReadLine();
+        Console.Write("単価を入力してください->");
+        item.Price = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("結果");
+        Console.WriteLine($"商品番号 = {item.ItemNo}");
+        Console.WriteLine($"商品名 = {item.ItemName}");
+        Console.WriteLine($"単価 = {item.Price}");
+    }
+}
---
+++ Ex_04_01_Lib/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex_04_01_Lib;
+/// <summary>
+/// 商品を表すクラス
+/// </summary>
+public class Item
+{
+    // 商品番号
+    public int ItemNo { get; set; }
+    // 商品名
+    public string? ItemName { get; set; }
+    // 単価
+    public int Price { get; set; }
+}
