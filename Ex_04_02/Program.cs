78663d854f9be91487a60407be5d0908286a2a08
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-11-07 09:16:29 +0900

    演習問題04-02を追加

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください → ");
+        int year = int.Parse(Console.ReadLine()!);
+        bool isLeap = DateTime.IsLeapYear(year);
+        if (isLeap)
+        {
+            Console.WriteLine($"{year}年は閏年です。");
+        }
+        else
+        {
+            Console.WriteLine($"{year}年は閏年ではありません。");
+        }
+    }
+}
