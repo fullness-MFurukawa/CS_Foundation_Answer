0296968d6716c20ade4877b179b2334258b247c4
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-11-10 10:24:07 +0900

    演習04-05追加

---
+++ Ex_04_05/Modules/TextFileAccessor.cs
@@ -0,0 +1,53 @@
+﻿namespace Ex_04_05.Modules;
+public class TextFileAccessor
+{
+    /// <summary>
+    /// 引数に指定されたファイルを開き、改行コード付きでList<T>内の文字列を書き込む
+    /// </summary>
+    /// <param name="fileName">ファイル名</param>
+    /// <param name="datas">書き込むデータ</param>
+    public void Write(string fileName, List<string> datas)
+    {
+        StreamWriter streamWriter = new StreamWriter(fileName);
+        try
+        {
+            foreach (var data in datas)
+            {
+                streamWriter.WriteLine(data);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            streamWriter.Close();
+        }
+    }
+    
+    /// <summary>
+    /// 引数に指定されたファイルを開き、１行単位で読み取ってコンソールに出力する
+    /// </summary>
+    /// <param name="fileName">ファイル名</param>
+
+    public void ReadAndPrint(string fileName)
+    {
+        StreamReader streamReader = new StreamReader(fileName);
+        try
+        {
+            string line = "";
+            while ((line = streamReader.ReadLine()!) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally {
+            streamReader.Close(); 
+        }
+    }
+}
---
+++ Ex_04_05/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ex_04_05.Modules;
+
+namespace Ex_04_05;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> datas = new List<string>();
+        while (true)
+        {
+            Console.Write("書き込むデータを入力してください->");
+            var data = Console.ReadLine();
+            if (data == "End")
+            {
+                break;
+            }
+            datas.Add(data!);
+        }
+        TextFileAccessor accessor = new TextFileAccessor();
+        accessor.Write("test.txt", datas);
+        Console.WriteLine("ファイルにデータを書き込みました。");
+        accessor.ReadAndPrint("test.txt");
+        Console.WriteLine("ファイルのデータを読み取りました。");
+    }
+}
